b8716f760e19ae42237b163fbe89ef2c0eda3d81
ZHANG Nan <zhn.i@hotmail.com>
2014-11-10 02:25:16 +0100

    add an UML

---
+++ Optimization-methods/Optimization-methods/binaryTree.h
@@ -0,0 +1,13 @@
+//
+//  binaryTree.h
+//  Optimization-methods
+//
+//  Created by ZN-MAC on 10/11/14.
+//  Copyright (c) 2014 ZHANG Nan. All rights reserved.
+//
+
+#ifndef Optimization_methods_binaryTree_h
+#define Optimization_methods_binaryTree_h
+
+
+#endif
--- Optimization-methods/Optimization-methods/main.cpp
+++ Optimization-methods/Optimization-methods/main.cpp
@@ -7,6 +7,7 @@
 //
 
 #include <iostream>
+#include "binaryTree.h"
 
 int main(int argc, const char * argv[]) {
     // insert code here...
