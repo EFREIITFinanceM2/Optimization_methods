1f968faf07f26a4704186fb95c3527cab8794085
HongyuLi <hongyu@hongyutekiMacBook-Air.local>
2014-11-11 17:13:09 +0100

    add CtoPositive

---
+++ Optimization-methods/Optimization-methods/CtoPositive.cpp
@@ -0,0 +1,9 @@
+//
+//  CtoPositive.cpp
+//  Optimization_methods_hongyu
+//
+//  Created by hongyu on 14/11/11.
+//  Copyright (c) 2014年 hongyu. All rights reserved.
+//
+
+#include "CtoPositive.h"
---
+++ Optimization-methods/Optimization-methods/CtoPositive.h
@@ -0,0 +1,14 @@
+//
+//  CtoPositive.h
+//  Optimization_methods_hongyu
+//
+//  Created by hongyu on 14/11/11.
+//  Copyright (c) 2014年 hongyu. All rights reserved.
+//
+
+#ifndef __Optimization_methods_hongyu__CtoPositive__
+#define __Optimization_methods_hongyu__CtoPositive__
+
+#include <stdio.h>
+
+#endif /* defined(__Optimization_methods_hongyu__CtoPositive__) */
