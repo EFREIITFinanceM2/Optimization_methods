fde40163179318db142553e2e9a96bad33ab1831
ZHANG Nan <zhn.i@hotmail.com>
2014-12-03 15:56:47 +0100

    init split

---
+++ Optimization-methods/Optimization-methods/split.cpp
@@ -0,0 +1,9 @@
+//
+//  split.cpp
+//  Optimization-methods
+//
+//  Created by ZN-MAC on 14/12/3.
+//  Copyright (c) 2014年 ZHANG Nan. All rights reserved.
+//
+
+#include "split.h"
---
+++ Optimization-methods/Optimization-methods/split.h
@@ -0,0 +1,14 @@
+//
+//  split.h
+//  Optimization-methods
+//
+//  Created by ZN-MAC on 14/12/3.
+//  Copyright (c) 2014年 ZHANG Nan. All rights reserved.
+//
+
+#ifndef __Optimization_methods__split__
+#define __Optimization_methods__split__
+
+#include <stdio.h>
+
+#endif /* defined(__Optimization_methods__split__) */
